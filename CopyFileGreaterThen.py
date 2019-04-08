7f005b1b92596452d72e55ae5e34b0ab71ef9e51
Kyle Wilkins <wilkins30@gmail.com>
2019-04-07 19:57:40 -0700

    updated to allow variables to be passed at runtime
    Updated both Dockerfile and CopyGreaterThen.py so that two bucket names and a minimum size can be passed as arguments into docker run. Also combined the two ending for loops so that it isn't going through the list of object keys more then once.

--- CopyFileGreaterThen.py
+++ CopyFileGreaterThen.py
@@ -1,35 +1,33 @@
 import boto3
+import sys
 from os import environ
 
 if __name__ == "__main__":
-    #Grabs bucket names from enviroment variable set in Dockerfile
-    DestBucketName = environ["DestBucket"]
-    SrcBucketName = environ["SrcBucket"]
-    MinSize = float(environ["MinSize"]) * 1024 * 1024
+    # Grabs bucket names and minsize from variable passed into docker
+    DestBucketName = sys.argv[1]
+    SrcBucketName = sys.argv[2]
+    MinSize = float(sys.argv[3]) * 1024 * 1024
 
-    #Grabs user credentials from enviroment variables
+
+    # Grabs user credentials from enviroment variables
     user = environ["username"]
     passkey = environ["passkey"]
 
+    # Get Buckets from s3 resource
     S3 = boto3.resource('s3', aws_access_key_id=user, aws_secret_access_key=passkey)
 
     S3Source = S3.Bucket(SrcBucketName)
     S3Destination = S3.Bucket(DestBucketName)
 
-
-    # Gets files that are larger then minimum size
-    FilesToMove = []
+    # Gets files that are larger then minimum size and copies them to the destination bucket
     for obj in S3Source.objects.all():
         if obj.size >= MinSize:
-            FilesToMove.append(obj.key)
+            CopySource = {
+                "Bucket" : obj.bucket_name,
+                "Key" : obj.key
+            }
+            S3.meta.client.copy(CopySource, DestBucketName, obj.key)
 
-    # Copies found files from SrcBucket to DestBucket
-    for key in FilesToMove:
-        CopySource = {
-            "Bucket" : SrcBucketName,
-            "Key" : key
-        }
-        S3.meta.client.copy(CopySource, DestBucketName, obj.key)
 
 
 
